8f4abd74371807050fd17d6a89b1a126b1d5c0fb
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2025-03-10 11:02:10 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
                     <img src="/images/icons/tsmc_logo.png" width="50" alt="">
                     {{-- <img src="/images/icons/iddrives_logo.png" width="50" alt=""> --}}
                     <img src="/images/icons/tz_logo.png" width="50" alt="">
-                    <img src="/images/icons/nt_logo.jpg" width="50" alt="">
+                    <img src="/images/icons/nt_logo.jpg" width="50" alt="" class="rounded-circle">
                     <!-- Button for sidebar toggle -->
                 </div>
                 <div class="sidebar-logo d-flex justify-content-between">
