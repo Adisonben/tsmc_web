47f7a674eb5356c58989e293de73220d96d266fb
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2025-04-28 16:19:39 +0700

    Update asssignTable.blade.php

--- resources/views/vehicleAssignment/asssignTable.blade.php
+++ resources/views/vehicleAssignment/asssignTable.blade.php
@@ -58,7 +58,7 @@
                                                     -ไม่พบข้อมูลการมอบหมาย-</td>
                                             @endif
                                             <td>
-                                                <button class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#assignCarModal{{ $index }}" {{ session('org_status') == 2 ? 'disabled' : '' }}>
+                                                <button class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#assignCarModal{{ $index }}">
                                                     <i class="bi bi-person-plus"></i>
                                                 </button>
                                             </td>
